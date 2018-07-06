241672d2492033985ffaaeda8f15a3d8a3924fae
Maksim Novoseltsev <nov.ms@bk.ru>
2018-07-06 18:48:55 +0400

    Добавил проект

--- js/works.js
+++ js/works.js
@@ -1,30 +1,51 @@
 var works = document.querySelector('.works');
 var worksRight = document.querySelector('.works__right');
-var bb = document.querySelector('.works__bb');
-var descBB = document.querySelector('.works__description-bb');
 var scrollWidth = window.innerWidth - document.documentElement.clientWidth;
 var allProjects = document.querySelector('.works__all-projects');
+var shadow = document.querySelectorAll('.works__shadow');
+
+var bb = document.querySelector('.works__bb');
+var descBB = document.querySelector('.works__description-bb');
+var cat = document.querySelector('.works__cat');
+var descCat = document.querySelector('.works__description-cat');
 
 bb.addEventListener('click', function () {
+    openDescription(bb, descBB);
+});
+
+cat.addEventListener('click', function () {
+    openDescription(cat, descCat);
+});
+
+for (var i = 0; i < shadow.length; i++) {
+    shadow[i].addEventListener('click', function() {
+        closeDescription (bb, descBB);
+        closeDescription (cat, descCat);
+    });
+}
+
+function openDescription (nameProject, descProjects) {
+
     works.scrollIntoView(false);
     Scrolled();
     worksRight.style.paddingRight = scrollWidth + 'px';
     allProjects.style.transitionDelay = '0s';
-    bb.style.transitionDelay = '0.3s';
+    nameProject.style.transitionDelay = '0.3s';
     allProjects.style.transform = 'rotateX(55deg) rotateZ(0deg) rotateY(0deg)';
-    bb.style.transform = 'rotateX(-55deg)';
+    nameProject.style.transform = 'rotateX(-55deg)';
     document.documentElement.style.overflow = 'hidden';
-    descBB.classList.add('works__description_open');
-})
+    descProjects.classList.add('works__description_open');
+}
 
-descBB.addEventListener('click', function () {
+function closeDescription (nameProject, descProject) {
+    console.log('close');
     works.scrollIntoView(false);
-    bb.style.transitionDelay = '0.3s';
+    nameProject.style.transitionDelay = '0.3s';
     allProjects.style.transitionDelay = '0.3s';
-    bb.style.transform = '';
+    nameProject.style.transform = '';
     allProjects.style.transform = '';
     document.documentElement.style.overflow = 'auto';
-    descBB.classList.remove('works__description_open');
+    descProject.classList.remove('works__description_open');
     worksRight.style.paddingRight = '';
-    bb.style.transitionDelay = '0s';
-})
+    nameProject.style.transitionDelay = '0s';
+}
